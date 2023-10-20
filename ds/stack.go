88f744d315a2bda150c480a58cbd4a2a648a732a
Xinecraft <kinnngg786@gmail.com>
2023-10-21 04:33:47 +0530

    Format

--- ds/stack.go
+++ ds/stack.go
@@ -3,39 +3,39 @@ package ds
 import "fmt"
 
 type Stack[T comparable] struct {
-	stack  LinkedList[T]
-	length int
+  stack  LinkedList[T]
+  length int
 }
 
 func (s *Stack[T]) Push(item T) {
-	s.length++
-	s.stack.Prepend(item)
+  s.length++
+  s.stack.Prepend(item)
 }
 
 func (s *Stack[T]) Pop() T {
-	if s.length <= 0 {
-		return *new(T)
-	}
+  if s.length <= 0 {
+    return *new(T)
+  }
 
-	s.length--
-	return s.stack.RemoveFirst()
+  s.length--
+  return s.stack.RemoveFirst()
 }
 
 func (s *Stack[T]) Peek(item T) T {
-	node := s.stack.head
+  node := s.stack.head
 
-	if node == nil {
-		return *new(T)
-	}
+  if node == nil {
+    return *new(T)
+  }
 
-	return node.value
+  return node.value
 }
 
 func (s *Stack[T]) Size(item T) int {
-	return s.length
+  return s.length
 }
 
 func (s Stack[T]) String() string {
-	str := fmt.Sprint(s.stack)
+  str := fmt.Sprint(s.stack)
   return str
 }
